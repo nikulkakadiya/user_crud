3080126fa6558e48bd0c3740157193b587b81417
nikul kakadiya <kakadiyamikul@gmail.com>
2023-05-04 00:11:52 +0530

    pagination code add pagination folder

--- controller/subjectController.js
+++ controller/subjectController.js
@@ -3,41 +3,45 @@ const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 exports.createSubject = catchAsync(async (req, res, next) => {
-    await Subject.create(req.body);
-    res.status(201).json({
-        status: "success",
-        message: "Add success Fully",
-    });
-})
+  await Subject.create(req.body);
+  res.status(201).json({
+    status: "success",
+    message: "Add success Fully",
+  });
+});
 
 exports.getAllSubject = catchAsync(async (req, res, next) => {
-    const result = await Subject.find({}, { _id: 1, name: 1, courseName: 1 });
-    res.status(201).json({
-        status: "success",
-        result
-    });
-})
+  const result = await Subject.find({}, { _id: 1, name: 1, courseName: 1 });
+  res.status(201).json({
+    status: "success",
+    result,
+  });
+});
 
 exports.updateSubject = catchAsync(async (req, res, next) => {
-    await Subject.findByIdAndUpdate(req.params.id, req.body);
-    res.status(201).json({
-        status: "success",
-        message: "update success Fully",
-    });
-})
+  await Subject.findByIdAndUpdate(req.params.id, req.body);
+  res.status(201).json({
+    status: "success",
+    message: "update success Fully",
+  });
+});
 
 exports.deleteSubject = catchAsync(async (req, res, next) => {
-    await Subject.findByIdAndDelete(req.params.id);
-    res.status(201).json({
-        status: "success",
-        message: "delete success Fully",
-    });
-})
+  await Subject.findByIdAndDelete(req.params.id);
+  res.status(201).json({
+    status: "success",
+    message: "delete success Fully",
+  });
+});
 
 exports.findSubjectById = catchAsync(async (req, res, next) => {
-   const data=await Subject.findById(req.params.id,{_id:1,name:1,courseName:1});
-   res.status(201).json({
+  const data = await Subject.findById(req.params.id, {
+    _id: 1,
+    name: 1,
+    courseName: 1,
+  });
+  res.status(201).json({
     status: "success",
-    data
+    data,
+  });
 });
-})
---
+++ pagination/code.js
@@ -0,0 +1,73 @@
+/*
+
+exports.getAllSubject = catchAsync(async (req, res, next) => {
+  const page = req.query.page || 1; // Set the default page number to 1
+  const limit = req.query.limit || 5; // Set the default limit to 10
+
+  const count = await Subject.countDocuments(); // Get the total number of documents in the collection
+  const totalPages = Math.ceil(count / limit); // Calculate the total number of pages
+
+  const skip = (page - 1) * limit; // Calculate the number of documents to skip
+
+  const result = await Subject.find({}, { _id: 1, name: 1, courseName: 1 })
+    .skip(skip)
+    .limit(limit);
+
+  res.status(200).json({
+    status: "success",
+    result,
+    currentPage: page,
+    totalPages,
+    totalResults: count,
+  });
+});
+
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectService {
+  private readonly apiUrl = 'http://localhost:3000/api/subjects';
+
+  constructor(private http: HttpClient) { }
+
+  getSubjects(page: number, limit: number): Observable<any> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', limit.toString());
+
+    return this.http.get(this.apiUrl, { params });
+  }
+}
+
+
+import { Component, OnInit } from '@angular/core';
+import { SubjectService } from '../subject.service';
+
+@Component({
+  selector: 'app-subject-list',
+  templateUrl: './subject-list.component.html',
+  styleUrls: ['./subject-list.component.css']
+})
+export class SubjectListComponent implements OnInit {
+  currentPage = 1;
+  totalPages = 1;
+  totalResults = 0;
+  subjects = [];
+
+  constructor(private subjectService: SubjectService) { }
+
+  ngOnInit(): void {
+    this.getSubjects();
+  }
+
+  getSubjects(): void {
+    this.subjectService.getSubjects(this.currentPage, 10)
+
+
+
+*/
