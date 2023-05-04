cb4d7389406765bc1e730a7b786738c43976447a
nikul kakadiya <kakadiyamikul@gmail.com>
2023-05-04 10:57:59 +0530

    example pagination

--- pagination/code.js
+++ pagination/code.js
@@ -1,5 +1,36 @@
 /*
 
+
+exports.getAllSubject = catchAsync(async (req, res, next) => {
+  const page = req.query.page || 1; // Set the default page number to 1
+  const limit = req.query.limit || 5; // Set the default limit to 10
+
+  const count = await Subject.countDocuments(); // Get the total number of documents in the collection
+  const totalPages = Math.ceil(count / limit); // Calculate the total number of pages
+
+  const skip = (page - 1) * limit; // Calculate the number of documents to skip
+
+  const result = await Subject.find({}, { _id: 1, name: 1, courseName: 1 })
+    .skip(skip)
+    .limit(limit);
+
+  // Calculate links to the previous and next pages
+  const prevPage = page > 1 ? `/subjects?page=${page - 1}&limit=${limit}` : null;
+  const nextPage = page < totalPages ? `/subjects?page=${page + 1}&limit=${limit}` : null;
+
+  res.status(200).json({
+    status: "success",
+    result,
+    currentPage: page,
+    totalPages,
+    totalResults: count,
+    firstPage: `/subjects?page=1&limit=${limit}`,
+    nextPage,
+    prevPage
+  });
+});
+
+
 exports.getAllSubject = catchAsync(async (req, res, next) => {
   const page = req.query.page || 1; // Set the default page number to 1
   const limit = req.query.limit || 5; // Set the default limit to 10
